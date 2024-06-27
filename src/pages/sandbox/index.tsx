d1bedea748c418d9343200ac4027fbf3c77d9fa7
Amanda Haldy <ahaldy@ebsco.com>
2024-06-27 13:56:34 -0400

    Adds page for the Sandbox

---
+++ src/pages/sandbox/index.tsx
@@ -0,0 +1,9 @@
+export default function Sandbox() {
+    return (<>
+        <h1>React Foundations Sandbox</h1>
+        <section>
+            <h2>What is this?</h2>
+            <p>This is a sandbox for testing out React features and libraries.</p>
+        </section>
+    </>);
+}
