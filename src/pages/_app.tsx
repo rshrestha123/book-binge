c4de914b721c40f5fb947cdb706e501efc85d9fa
Amanda Haldy <ahaldy@ebsco.com>
2024-08-01 14:30:10 -0400

    SearchBar update and international provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,8 +2,11 @@ import "../global.scss";
 import type { AppProps } from "next/app";
 import { config } from "@fortawesome/fontawesome-svg-core";
 import "@fortawesome/fontawesome-svg-core/styles.css";
+import {IntlProvider} from 'react-intl'
 config.autoAddCss = false;
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const locale = "en";
+
+  return <IntlProvider locale={locale} messages={{}}><Component {...pageProps} /></IntlProvider>;
 }
