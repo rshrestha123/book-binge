6d0c19223075423ca1a89fc809e65c7dd67ff61f
Rohit Shrestha <rshrestha@ebsco.com>
2024-09-30 16:11:23 -0600

    Building navigation bar with style

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -1,12 +1,29 @@
 import Navlink from "@/components/navbar/navlinks";
+import styles from "./index.module.scss";
+import Link from "next/link";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBookOpen } from "@fortawesome/free-solid-svg-icons/faBookOpen";
 
 export default function Navbar() {
-    return(<div>
-        <Navlink displayText={"Book Binge"} linkPath={"/"}/>
-        <ul>
-            <li><Navlink displayText={"Search"} linkPath={"/"}/></li>
-            <li><Navlink displayText={"Want to read"} linkPath={"want-to-read"} /></li>
-            <li><Navlink displayText={"Finished reading"} linkPath={"finished-reading"} /></li>
-        </ul>
-    </div>)
+  return (
+    <div className={styles.navbar}>
+      <div>
+        <Link href={"/"} className={styles.appName}>
+          <FontAwesomeIcon icon={faBookOpen} className={styles.icon} />
+          Book Binge
+        </Link>
+      </div>
+      <ul>
+        <li>
+          <Navlink displayText={"Search"} linkPath={"/"} />
+        </li>
+        <li>
+          <Navlink displayText={"Want to read"} linkPath={"want-to-read"} />
+        </li>
+        <li>
+          <Navlink displayText={"Finished reading"} linkPath={"finished-reading"} />
+        </li>
+      </ul>
+    </div>
+  );
 }
